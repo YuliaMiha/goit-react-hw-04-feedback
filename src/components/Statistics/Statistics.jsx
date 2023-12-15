d59fb54e2dffb7f86daff5dc6ace1636ed8d8e3a
Yulia Palamarchuk <83129717+YuliaMiha@users.noreply.github.com>
2023-12-16 00:52:18 +0200

    summary fix

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -5,14 +5,17 @@ import s from './Statistics.module.css';
 function Statistics({ good, neutral, bad, total, positivePercentage }) {
     // console.log(positivePercentage);
     
-    return total ?  <div className={s.statistics}>
-            <p className={s.statistics__text}>Good: {good}</p>
-            <p className={s.statistics__text}>Neutral: {neutral}</p>
-            <p className={s.statistics__text}>Bad: { bad}</p>
-            <p  className={s.statistics__text}>Total: {total}</p>
-            <p className={s.statistics__text}>Positive feedback: {positivePercentage === 'NaN' ? "0" : positivePercentage}%</p>
-      </div> : <Notification message="There is no feedback" />
-       
+    return total === 0 ? (
+        <Notification message="There is no feedback" />
+      ) : (
+        <div className={s.statistics}>
+          <p className={s.statistics__text}>Good: {good}</p>
+          <p className={s.statistics__text}>Neutral: {neutral}</p>
+          <p className={s.statistics__text}>Bad: {bad}</p>
+          <p className={s.statistics__text}>Total: {total}</p>
+          <p className={s.statistics__text}>Positive feedback: {positivePercentage === 'NaN' ? "0" : positivePercentage}%</p>
+        </div>
+      )
     
 }
 
